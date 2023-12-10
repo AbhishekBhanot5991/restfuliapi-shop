eb7418d6d0c7b8554648a137d082869d33332aeb
AbhishekBhanot5991 <abhishekbhanot5991@gmail.com>
2023-12-10 21:57:50 +0530

    file name changed

--- routes/products.js
+++ routes/products.js
@@ -44,4 +44,21 @@ router.post('/', async (req, res) => {
   }
 });
 
+// Get a particular product by ID
+router.get('/:id', async (req, res) => {
+  const productId = req.params.id;
+
+  try {
+    const product = await Product.findById(productId);
+
+    if (!product) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
+
+    res.json(product);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error fetching product', error: error.message });
+  }
+});
 module.exports = router;
