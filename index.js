8021a743a2ec553f26eb005f3a6d52b3e309a1b3
AbhishekBhanot5991 <abhishekbhanot5991@gmail.com>
2023-12-11 15:22:07 +0530

    done

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ const productsRouter = require('./routes/products');
 app.use('/api/products', productsRouter);
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true,  retryWrites: true, w: 'majority'  });
-// mongoose.connect(process.env.MONGODB_URI);
+// mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true,  retryWrites: true, w: 'majority'  });
+mongoose.connect(process.env.MONGODB_URI);
 const db = mongoose.connection;
 // db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.on('error', (err) => {
