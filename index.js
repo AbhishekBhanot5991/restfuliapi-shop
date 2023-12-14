24f864e531acd704ebb7466f48733073c74a7299
AbhishekBhanot5991 <abhishekbhanot5991@gmail.com>
2023-12-14 18:50:50 +0530

    updated code

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 // app.use(bodyParser.json());
 app.use(express.json());
+app.use(bodyParser.json()); 
 app.use(passport.initialize());
 
 // Routes
