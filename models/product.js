2be6cc8d78eca5dfa502e2f9311bfac5ec8084c3
AbhishekBhanot5991 <abhishekbhanot5991@gmail.com>
2023-12-14 14:16:15 +0530

    updated

--- models/product.js
+++ models/product.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const productSchema = new mongoose.Schema({
   name: { type: String, required: true },
   price: { type: Number, required: true },
+  category: { type: Number, required: true },
   description: { type: String },
   imageUrl: { type: String }, // Add a field for storing image paths
 });
