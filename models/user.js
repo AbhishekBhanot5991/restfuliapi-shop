aac82bb56e833f241d726179ebc0603ae2f6718c
AbhishekBhanot5991 <abhishekbhanot5991@gmail.com>
2023-12-15 15:39:59 +0530

    update

--- models/user.js
+++ models/user.js
@@ -5,6 +5,7 @@ const bcrypt = require('bcrypt');
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  confirmPassword: { type: String, required: true },
 });
 
 userSchema.methods.generateHash = async function (password) {
